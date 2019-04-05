534991b3c3a3963d60853719fe9d91023fe90c36
Rohit Saxena <rohitmsaxena@gmail.com>
2019-04-04 21:33:14 -0500

    implemented getAdvertiserByName using request params instead

--- src/main/java/com/iheartdemo/advertising/controllers/AdvertisingController.java
+++ src/main/java/com/iheartdemo/advertising/controllers/AdvertisingController.java
@@ -4,10 +4,7 @@ import com.iheartdemo.advertising.models.Advertiser;
 import com.iheartdemo.advertising.service.AdvertisingService;
 import com.iheartdemo.advertising.serviceImpl.AdvertisingServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 
@@ -33,8 +30,8 @@ public class AdvertisingController {
         return advertisingService.getAllAdvertisers();
     }
 
-    @GetMapping("/getAdvertiserByName/{name}")
-    ArrayList<Advertiser> getAdveriserByName(@PathVariable("name") String name) {
+    @GetMapping("/getAdvertiserByName")
+    ArrayList<Advertiser> getAdveriserByName(@RequestParam("name") String name) {
         return advertisingService.getAdvertiser(name);
     }
 }
