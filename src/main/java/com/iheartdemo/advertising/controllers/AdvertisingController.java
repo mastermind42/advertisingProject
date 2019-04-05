e060a3a19282b6e412ff34c5db51d1486bdf798b
Rohit Saxena <rohitmsaxena@gmail.com>
2019-04-04 21:29:37 -0500

    implemented getAdvertiserByName

--- src/main/java/com/iheartdemo/advertising/controllers/AdvertisingController.java
+++ src/main/java/com/iheartdemo/advertising/controllers/AdvertisingController.java
@@ -5,11 +5,14 @@ import com.iheartdemo.advertising.service.AdvertisingService;
 import com.iheartdemo.advertising.serviceImpl.AdvertisingServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.ArrayList;
 
 @RestController
+@RequestMapping("/api/advertiser")
 public class AdvertisingController {
 
     private final AdvertisingService advertisingService;
@@ -28,5 +31,10 @@ public class AdvertisingController {
     @GetMapping("/getAllAdvertisers")
     ArrayList<Advertiser> getAllAdvertisers() {
         return advertisingService.getAllAdvertisers();
+    }
+
+    @GetMapping("/getAdvertiserByName/{name}")
+    ArrayList<Advertiser> getAdveriserByName(@PathVariable("name") String name) {
+        return advertisingService.getAdvertiser(name);
     }
 }
