a9e86906d64faee2db3e967112216e2bdeeba3ef
Rohit Saxena <rohitmsaxena@gmail.com>
2019-04-04 21:24:05 -0500

    implemented getAllAdvertisers endpoint

--- src/main/java/com/iheartdemo/advertising/controllers/AdvertisingController.java
+++ src/main/java/com/iheartdemo/advertising/controllers/AdvertisingController.java
@@ -1,13 +1,32 @@
 package com.iheartdemo.advertising.controllers;
 
+import com.iheartdemo.advertising.models.Advertiser;
+import com.iheartdemo.advertising.service.AdvertisingService;
+import com.iheartdemo.advertising.serviceImpl.AdvertisingServiceImpl;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
 
 @RestController
 public class AdvertisingController {
 
-    @GetMapping("/")
-    String home() {
+    private final AdvertisingService advertisingService;
+
+    @Autowired
+    public AdvertisingController(AdvertisingService advertisingService) {
+        this.advertisingService = advertisingService;
+    }
+
+    //todo delete this endpoint
+    @GetMapping("/test")
+    String test() {
         return "Hello World!";
+    }
+
+    @GetMapping("/getAllAdvertisers")
+    ArrayList<Advertiser> getAllAdvertisers() {
+        return advertisingService.getAllAdvertisers();
     }
 }
